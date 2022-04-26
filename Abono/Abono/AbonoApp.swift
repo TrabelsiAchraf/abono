5069a7f2a5402089a4e6407f90f6c8700b4e5e5e
Achraf Trabelsi <trabelsiachraf.mobile@gmail.com>
2022-04-27 00:15:22 +0200

    Init Project

---
+++ Abono/Abono/AbonoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbonoApp.swift
+//  Abono
+//
+//  Created by Achraf Trabelsi on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbonoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
