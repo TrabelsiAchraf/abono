4ac5ea01191e10df21ddd56178314d09f6ac4692
Achraf Trabelsi <trabelsiachraf.mobile@gmail.com>
2022-08-02 17:08:56 +0100

    Extract Appearance to AppAppearance file

--- Abono/AbonoApp.swift
+++ Abono/AbonoApp.swift
@@ -38,27 +38,3 @@ struct AbonoApp: App {
         }
     }
 }
-
-enum Appearance: String {
-    case system, light, dark
-    var value: ColorScheme? {
-        switch self {
-        case .system:
-            return nil
-        case .light:
-            return .light
-        case .dark:
-            return .dark
-        }
-    }
-    var text: String {
-        switch self {
-        case .system:
-            return "System"
-        case .light:
-            return "Light"
-        case .dark:
-            return "Dark"
-        }
-    }
-}
---
+++ Abono/AppAppearance.swift
@@ -0,0 +1,32 @@
+//
+//  AppAppearance.swift
+//  Abono
+//
+//  Created by Achraf Trabelsi on 02/08/2022.
+//
+
+import SwiftUI
+
+enum Appearance: String {
+    case system, light, dark
+    var value: ColorScheme? {
+        switch self {
+        case .system:
+            return nil
+        case .light:
+            return .light
+        case .dark:
+            return .dark
+        }
+    }
+    var text: String {
+        switch self {
+        case .system:
+            return "System"
+        case .light:
+            return "Light"
+        case .dark:
+            return "Dark"
+        }
+    }
+}
