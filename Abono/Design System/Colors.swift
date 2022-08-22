18f8efd3a9ff3929cae9d2ddddcebbba71ba53eb
Achraf Trabelsi <trabelsiachraf.mobile@gmail.com>
2022-08-22 14:15:15 +0200

    Create an app design system : add Colors

---
+++ Abono/Design System/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Abono
+//
+//  Created by Achraf Trabelsi on 22/08/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let defaultBackground: Color = Color("background")
+    static let card: Color = Color("card")
+    static let second: Color = Color("second")
+    static let progressView: Color = Color("progressView")
+    static let amountTitle: Color = Color("amountTitle")
+}
