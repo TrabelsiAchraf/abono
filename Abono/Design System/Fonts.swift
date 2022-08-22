6f3e1302147e98b941e15e474ef723f63de55b5f
Achraf Trabelsi <trabelsiachraf.mobile@gmail.com>
2022-08-22 14:25:08 +0200

    Add Font to app design system

---
+++ Abono/Design System/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  Abono
+//
+//  Created by Achraf Trabelsi on 22/08/2022.
+//
+
+import SwiftUI
+
+extension Font {
+    static let small: Font = .system(size: 12)
+    static let medium1: Font = .system(size: 14)
+    static let medium2: Font = .system(size: 16)
+    static let large: Font = .system(size: 18)
+    static let xlarge: Font = .system(size: 26)
+}
