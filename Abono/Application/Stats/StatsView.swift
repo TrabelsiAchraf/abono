91fb9759a543d2480a31beb2b66f8ae7dbe9aa74
Achraf Trabelsi <trabelsiachraf.mobile@gmail.com>
2022-08-27 23:57:02 +0200

    Add StatsView : under construction

---
+++ Abono/Application/Stats/StatsView.swift
@@ -0,0 +1,26 @@
+//
+//  StatsView.swift
+//  Abono
+//
+//  Created by Achraf Trabelsi on 27/08/2022.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        VStack(spacing: 30) {
+            Text("Stats View")
+                .font(.largeTitle)
+                .bold()
+            Text("Under Construction 🚧")
+                .bold()
+        }
+    }
+}
+
+struct StatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatsView()
+    }
+}
