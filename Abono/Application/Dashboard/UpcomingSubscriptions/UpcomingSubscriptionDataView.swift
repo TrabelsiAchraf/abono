aa32856901929c81d405fafd7263f54ee0c54e8c
Achraf Trabelsi <trabelsiachraf.mobile@gmail.com>
2022-08-28 01:32:01 +0200

    Use immutable variable : let id: UUID

--- Abono/Application/Dashboard/UpcomingSubscriptions/UpcomingSubscriptionDataView.swift
+++ Abono/Application/Dashboard/UpcomingSubscriptions/UpcomingSubscriptionDataView.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct UpcomingSubscriptionDataView: Equatable, Identifiable {
-    var id = UUID()
+    let id: UUID
     let name: String
     let icon: String
     let amount: String
