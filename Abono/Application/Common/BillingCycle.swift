5838f92000ad60706d1c24d1c995c6afa8c7ab0e
Achraf Trabelsi <trabelsiachraf.mobile@gmail.com>
2022-08-27 23:56:18 +0200

    Add common enums : BillingCycle and SubscriptionCategory

---
+++ Abono/Application/Common/BillingCycle.swift
@@ -0,0 +1,12 @@
+//
+//  BillingCycle.swift
+//  Abono
+//
+//  Created by Achraf Trabelsi on 27/08/2022.
+//
+
+enum BillingCycle: String {
+    case montly = "Monthly"
+    case weekly = "Weekly"
+    case annual = "Annual"
+}
---
+++ Abono/Application/Common/SubscriptionCategory.swift
@@ -0,0 +1,19 @@
+//
+//  SubscriptionCategory.swift
+//  Abono
+//
+//  Created by Achraf Trabelsi on 27/08/2022.
+//
+
+enum SubscriptionCategory: String {
+    case entertainment = "Entertainment"
+    case photo = "Photo"
+    case video = "Video"
+    case eCommerce = "E-Commerce"
+    case education = "Education"
+    case food = "Food & Drinks"
+    case music = "Music"
+    case transportation = "Transportation"
+    case games = "Games"
+    case other = "Other"
+}
